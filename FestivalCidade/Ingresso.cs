2b9252d81abf6c8e2b3b9b89f18eb843b41ef84c
Kauan <kauandiogodapazsilva16@gmail.com>
2026-08-25 16:28:02 -0300

    feat: Criando a Classe Ingresso e a função CalcularCustoBruto()

---
+++ FestivalCidade/Ingresso.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FestivalCidade
+{
+    public class Ingresso
+    {
+        public string NomeFa { get; set; }
+        public int DiasShow { get; set; }
+        public string Cupom { get; set; }
+        public int PontosFidelidade { get; set; }
+
+        public double CalcularCustoBruto()
+        {
+            return 15.00 + (120.00 * DiasShow);
+        }
+
+
+
+    }
+}
